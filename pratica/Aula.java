6cc11c740d95287588c0f32cc514e9bf611b76ae
Geovani Gleidson <64352434+geovanig@users.noreply.github.com>
2020-07-12 10:06:19 -0300

    Arquivos adicionados via upload

---
+++ pratica/Aula.java
@@ -0,0 +1,35 @@
+package com.pratica;
+
+import javax.swing.JOptionPane;
+
+public class Aula {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "          Olá, vamos jogar!");
+		JOptionPane.showMessageDialog(null, "Irei escolher um número entre 0 e 100\n"
+				                       + "        Você tentará adivinhar");
+		
+		int number = (int)(Math.random() * 100);
+		int palpite = Integer.parseInt(JOptionPane.showInputDialog("Já escolhi meu número, qual você acha que é?"));
+		
+		int count = 1
+				;
+		while (true) {
+			if (palpite > number) {
+				count ++;
+				palpite = Integer.parseInt(JOptionPane.showInputDialog("Não é esse número, chute mais baixo"));
+			}else if (palpite < number) {
+				count ++;
+				palpite = Integer.parseInt(JOptionPane.showInputDialog("Não é esse número, tente chutar mais alto"));
+			}else {
+				JOptionPane.showMessageDialog(null, "                  Exato! Escolhi o número "+number+
+						                      "\nObrigado por jogar, você acertou com "+count+" tentativas");
+				break;
+			}	
+		}
+	}
+	
+}
+
+
