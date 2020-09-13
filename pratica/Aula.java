4e9d74f4a893952f4efd1c1f4aceb7f3030b8e8e
Geovani Gleidson <64352434+geovanig@users.noreply.github.com>
2020-09-13 09:30:28 -0300

    Atualização com um bloco try/catch

--- pratica/Aula.java
+++ pratica/Aula.java
@@ -2,7 +2,7 @@ package com.pratica;
 
 import javax.swing.JOptionPane;
 
-public class Aula {
+public class Adivinha {
 
 	public static void main(String[] args) {
 		
@@ -10,26 +10,29 @@ public class Aula {
 		JOptionPane.showMessageDialog(null, "Irei escolher um número entre 0 e 100\n"
 				                       + "        Você tentará adivinhar");
 		
-		int number = (int)(Math.random() * 100);
-		int palpite = Integer.parseInt(JOptionPane.showInputDialog("Já escolhi meu número, qual você acha que é?"));
-		
-		int count = 1
-				;
-		while (true) {
-			if (palpite > number) {
-				count ++;
-				palpite = Integer.parseInt(JOptionPane.showInputDialog("Não é esse número, chute mais baixo"));
-			}else if (palpite < number) {
-				count ++;
-				palpite = Integer.parseInt(JOptionPane.showInputDialog("Não é esse número, tente chutar mais alto"));
-			}else {
-				JOptionPane.showMessageDialog(null, "                  Exato! Escolhi o número "+number+
-						                      "\nObrigado por jogar, você acertou com "+count+" tentativas");
-				break;
-			}	
+		try {
+			int number = (int)(Math.random() * 100);
+			int palpite = Integer.parseInt(JOptionPane.showInputDialog("Já escolhi meu número, qual você acha que é?"));
+			
+			int count = 1;
+			
+			while (true) {
+				if (palpite > number) {
+					count ++;
+					palpite = Integer.parseInt(JOptionPane.showInputDialog("Não é esse número, chute mais baixo"));
+				}else if (palpite < number) {
+					count ++;
+					palpite = Integer.parseInt(JOptionPane.showInputDialog("Não é esse número, tente chutar mais alto"));
+				}else {
+					JOptionPane.showMessageDialog(null, "                  Exato! Escolhi o número "+number+
+							                      "\nObrigado por jogar, você acertou com "+count+" tentativas");
+					break;
+				}	
+			}
+		} catch (Exception e) {
+			System.out.println("Ocorreu um erro ao digitar o número, reinicie o programa!");
 		}
+		
 	}
 	
 }
-
-
